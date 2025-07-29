1152a77f0655a026c0613921c5fa451e05c37993
Siska Putri <91827161+Siputsss@users.noreply.github.com>
2025-07-29 17:06:11 +0700

    feat: textfield (injectTextEditing)

--- lib/ui_screens/a.product/c.product_add/a.product_add.data.dart
+++ lib/ui_screens/a.product/c.product_add/a.product_add.data.dart
@@ -6,4 +6,8 @@ class ProductAddData {
   final rxCounter = 0.inj();
 
   final rxRandom = Prov.sample.st.rxRandom;
+
+  final rxName = RM.injectTextEditing(validators: [Validate.isNotEmpty, Validate.fullName]);
+  final rxPrice = RM.injectTextEditing(validators: [Validate.isNotEmpty, Validate.isNumeric]);
+  final rxQty = RM.injectTextEditing(validators: [Validate.isNotEmpty, Validate.isNumeric]);
 }
