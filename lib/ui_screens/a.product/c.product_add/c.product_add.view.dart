9757303398749105e7a9af63f928486462879eb9
Siska Putri <91827161+Siputsss@users.noreply.github.com>
2025-08-01 01:12:39 +0700

    feat: add image (state)

--- lib/ui_screens/a.product/c.product_add/c.product_add.view.dart
+++ lib/ui_screens/a.product/c.product_add/c.product_add.view.dart
@@ -14,14 +14,26 @@ class ProductAddView extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
-                onPressed: () async {
-                  _dt.rxPickedImage.st = await ImagePicker().pickImage(source: ImageSource.gallery);
-                  debugPrint(_dt.rxPickedImage.st?.mimeType);
-                  debugPrint(_dt.rxPickedImage.st?.name);
-                  debugPrint(_dt.rxPickedImage.st?.path);
-                },
-                child: Text("Pick Image"),
+              OnReactive(
+                () => Column(
+                  children: [
+                    _dt.rxPickedImage.st == null
+                        ? Card(
+                            child: SizedBox(height: 100, width: 100, child: Center(child: Text('No Image'))),
+                          )
+                        : SizedBox(height: 100, width: 100, child: Image.network('${_dt.rxPickedImage.st?.path}')),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () async {
+                        _dt.rxPickedImage.st = await ImagePicker().pickImage(source: ImageSource.gallery);
+                        debugPrint(_dt.rxPickedImage.st?.mimeType);
+                        debugPrint(_dt.rxPickedImage.st?.name);
+                        debugPrint(_dt.rxPickedImage.st?.path);
+                      },
+                      child: Text("Pick Image"),
+                    ),
+                  ],
+                ),
               ),
               const SizedBox(height: 20),
               OnFormBuilder(
